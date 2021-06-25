e38b7fd1aa447bf85b7b7e438907e25d5801de5f
Radifan Mardhi Prana <43719706+radifanmp@users.noreply.github.com>
2021-06-26 01:29:21 +0700

    5.State

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
-import Props from "./props";
+import State from "./state";
 import reportWebVitals from "./reportWebVitals";
 
 ReactDOM.render(
   <React.StrictMode>
-    <Props />
+    <State />
   </React.StrictMode>,
   document.getElementById("root")
 );
---
+++ src/state.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+function State() {
+  //init State
+
+  //Create Function Add
+
+  //Create Function Less
+
+  return (
+    // Code Inside div
+    <div></div>
+  );
+}
+
+export default State;
