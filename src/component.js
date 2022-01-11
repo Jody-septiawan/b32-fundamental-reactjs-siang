fd0e6333bb676f51a0a6deaf598a9a713810114d
Ilham Fathullah <58564312+ilhamfat04@users.noreply.github.com>
2022-01-11 08:22:25 +0700

    Update component.js

--- src/component.js
+++ src/component.js
@@ -1,26 +1,14 @@
 import React from "react";
 
 // import component Header
-import Header from "./components/header";
 
 function Component() {
   return (
     //   Code Here
-    <div>
-      <Header />
-      <Content />
-    </div>
+    <div></div>
   );
 }
 
 // Create a new component here
-function Content() {
-  return (
-    <div>
-      <h1>This is Content</h1>
-      <button>Click Me</button>
-    </div>
-  )
-}
 
 export default Component;
