1df17da6c33c002eabffe9670cd5c352a0979e4d
Jody-septiawan <jody.septiawan5@gmail.com>
2022-03-28 14:08:18 +0700

    Embedding Expression

--- src/embedExpression.js
+++ src/embedExpression.js
@@ -1,13 +1,21 @@
-import React from "react";
+import React from 'react';
 
 function EmbedExpression() {
   //Create function here
+  function getMajor() {
+    return 'Full-Stack';
+  }
 
   //Create a variable here
+  const companyName = 'Dumbways.Id';
 
   return (
     // Code Inside div
-    <div></div>
+    <div>
+      <p>
+        Welcome To {companyName} Class {getMajor()}
+      </p>
+    </div>
   );
 }
 
