87a21381783c80f5c8e56e42a668a5671cd8116e
Jody-septiawan <jody.septiawan5@gmail.com>
2022-03-28 14:33:40 +0700

    Props

--- src/components/list.js
+++ src/components/list.js
@@ -1,1 +1,12 @@
-// code Here
+import React from 'react';
+
+export default function List(props) {
+  console.log(props);
+  return (
+    <div>
+      <h1>{props.name}</h1>
+      <h4>{props.year}</h4>
+      <button onClick={props.onClick}>Click Me</button>
+    </div>
+  );
+}
--- src/props.js
+++ src/props.js
@@ -1,11 +1,44 @@
-import React from "react";
+import React from 'react';
 
 //Import Components
+import List from './components/list';
 
 function Props() {
+  const author = {
+    name: 'Dani D',
+    address: 'Semarang',
+  };
+
+  const onClick = () => {
+    alert('You click button');
+  };
+
+  const peoples = [
+    {
+      name: 'Allif',
+    },
+    {
+      name: 'Silvy',
+    },
+    {
+      name: 'Afif',
+    },
+  ];
+
   return (
     // Code Inside div
-    <div></div>
+    <div>
+      <img
+        src="https://images.unsplash.com/photo-1489824904134-891ab64532f1?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1631&q=80"
+        alt=""
+        style={{ width: '500px' }}
+      />
+
+      <List name="MBW" tax={false} year={2015} author={author} />
+      <List name="Mercedes-Benz" />
+      <List name="Lamborghini" onClick={onClick} />
+      <List name="Ducati" people={peoples[1]} />
+    </div>
   );
 }
 
