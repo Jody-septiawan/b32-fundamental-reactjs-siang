64bd28ba13d33c01c0b618ccd1373b26fa366d6a
Jody-septiawan <jody.septiawan5@gmail.com>
2022-03-28 14:15:01 +0700

    Event

--- src/event.js
+++ src/event.js
@@ -1,11 +1,24 @@
-import React from "react";
+import React from 'react';
 
 function Event() {
   //Create function here
+  function Greeting() {
+    alert('Hello everyone have a nice day');
+  }
 
   return (
     // Code Inside div
-    <div></div>
+    <div>
+      <p onMouseOver={() => alert('Click paragraph element')}>
+        If you press 'Click Here' then an alert will appear
+      </p>
+      <button onClick={() => alert('Hello full-stack bootcamp participants')}>
+        Click Here
+      </button>
+
+      <p>If you press 'Greeting' then an alert will appear</p>
+      <button onClick={Greeting}>Greeting</button>
+    </div>
   );
 }
 
