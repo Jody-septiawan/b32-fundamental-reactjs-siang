4210d42a6b246299bf84c35732a876342aa94033
Jody-septiawan <jody.septiawan5@gmail.com>
2022-03-28 14:54:47 +0700

    Conditional Rendering

--- src/conditionRendering.js
+++ src/conditionRendering.js
@@ -1,14 +1,44 @@
-import React from "react";
+import React, { useState } from 'react';
 
 //Create Component PrivatePage
+const PrivatePage = (props) => {
+  return (
+    <div>
+      <h1>Welcome</h1>
+      <button onClick={props.logout}>logout</button>
+    </div>
+  );
+};
 
 //Create Component GuestPage
+const GuestPage = (props) => {
+  return (
+    <div>
+      <h1>Please Login</h1>
+      <button onClick={props.login}>login</button>
+    </div>
+  );
+};
 
 function ConditionRenderin() {
   //init State
+  const [isLogin, setIsLogin] = useState(false);
+  console.log(isLogin);
+
+  if (isLogin == true) {
+    // render PrivatePage component
+  } else {
+    // render GuestPage component
+  }
+
   return (
-    // Code Inside div
-    <div>{/* conditional logic rendering */}</div>
+    <div>
+      {isLogin == true ? (
+        <PrivatePage logout={() => setIsLogin(!isLogin)} />
+      ) : (
+        <GuestPage login={() => setIsLogin(!isLogin)} />
+      )}
+    </div>
   );
 }
 
