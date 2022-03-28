5c3f6c21658efc51bffba8bd5d6c016cadf2b5fa
Jody-septiawan <jody.septiawan5@gmail.com>
2022-03-28 14:44:44 +0700

    State

--- src/state.js
+++ src/state.js
@@ -1,15 +1,30 @@
-import React from "react";
+import React, { useState } from 'react';
 
 function State() {
   //init State
 
+  const [counter, setCounter] = useState(10);
+
+  console.log(counter);
+
   //Create Function Add
+  const Add = () => {
+    setCounter(counter + 1);
+  };
 
   //Create Function Less
+  const Less = () => {
+    setCounter(counter - 1);
+  };
 
   return (
     // Code Inside div
-    <div></div>
+    <div>
+      <h2>{counter}</h2>
+
+      <button onClick={Add}>Add</button>
+      <button onClick={Less}>Less</button>
+    </div>
   );
 }
 
